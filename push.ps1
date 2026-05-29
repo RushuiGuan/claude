b3ef0a87d07bfb75c7b4ac881bee539c54910c66
Rushui Guan <rushui@outlook.com>
2026-05-28 21:13:14 -0400

    update script

--- push.ps1
+++ push.ps1
@@ -1,2 +1,3 @@
 Get-Item ~/.claude/skills/alba-* | remove-item -Force -Recurse;
 Copy-Item $PSScriptRoot/skills/* ~/.claude/skills -recurse -Force
+Copy-Item $PSScriptRoot/skills/settings.json ~/.claude/settings.json -Force
