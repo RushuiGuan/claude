d38442e4821722ed078cd75c7f4fdb4a85c5805d
Rushui <rushui@outlook.com>
2026-05-30 14:19:54 -0400

    cleanup

--- pull.ps1
+++ pull.ps1
@@ -1,2 +1,1 @@
 Copy-Item ~/.claude/skills/* $PSScriptRoot/skills -recurse -Force
-Copy-Item ~/.claude/settings.json $PSScriptRoot -Force
--- push.ps1
+++ push.ps1
@@ -1,3 +1,2 @@
 Get-Item ~/.claude/skills/alba-* | remove-item -Force -Recurse;
 Copy-Item $PSScriptRoot/skills/* ~/.claude/skills -recurse -Force
-Copy-Item $PSScriptRoot/settings.json ~/.claude/settings.json -Force
